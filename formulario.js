2a74e9d2d54a61a2b73e04a9f885ca303b66d267
Fernando Gouveia <gouveiaramos.me@gmail.com>
2019-03-22 12:41:09 +0000

    commit 3

--- formulario.js
+++ formulario.js
@@ -8,5 +8,13 @@ function criarTabela(conteudo) {
   var thead = document.createElement("thead");
   var tbody=document.createElement("tbody");
   var thd=function(i){return (i==0)?"th":"td";};
+  for (var i=0;i<conteudo.length;i++) {
+    var tr = document.createElement("tr");
+    for(var o=0;o<conteudo[i].length;o++){
+      var t = document.createElement(thd(i));
+      var texto=document.createTextNode(conteudo[i][o]);
+      t.appendChild(texto);
+      tr.appendChild(t);
+    }
 
 
